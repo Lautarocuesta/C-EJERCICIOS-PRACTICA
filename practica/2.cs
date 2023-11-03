d8b3f2b205322beade90dae4c58de84e71412f69
Lautaro Cuesta <d47305409@instituto.itr.edu.ar>
2023-11-03 12:11:04 -0300

    2

---
+++ practica/2.cs
@@ -0,0 +1,25 @@
+using System; 
+using System.Collections.Generic;
+
+
+public class Program { 
+//evaluateRpn (5 3 4 * + 7 -) -> 10
+//3x4=12; 12+5=17, 17-7= 10
+ int Ntp(){
+    var pila = new Stack<int>();
+     int[] numeros; 
+     numeros = new int[5 3];
+    
+    Console.WriteLine("Pila original:");
+    console.ReadLine()
+    }
+
+   
+ }
+  public static void Main() {
+    Stack<int> pila = new Stack<int>();
+    Program obj = new Program();
+    obj.Ntp
+ }
+
+  
