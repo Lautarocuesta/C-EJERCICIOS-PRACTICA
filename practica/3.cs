39ad497ac74251fcc7a473b0868f7691bffd279c
Cuesta Lautaro <d47305409@alumnos.itr.edu.ar>
2023-11-16 16:40:09 -0300

    2cs

---
+++ practica/3.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+class Program{
+    public static void colae(){
+      Queue<int> callerIds = new Queue<int>();
+      Console.WriteLine("ingrese el numero que queira ingresar:");
+      int cante=int.Parse(console.Readline());
+      callerIds.Enqueue(value);
+      
+    }
+  console.writeline("numeros par en cola");
+  foreach(int nro in callerIds){
+  if(nro%2==0){
+    console.writeline(nro);
+  }
+  }
+  
+ 
+  for(int i=0; i< cante; i++){
+    
+  }
+
+public static void Main () {
+colae();
+}
